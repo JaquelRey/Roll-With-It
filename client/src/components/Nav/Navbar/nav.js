94727d28bc909620c1cb4f4fefc004d8ba37238e
Sophia R <sophie.g.rankin@gmail.com>
2022-10-25 20:37:41 -0400

    started home page

---
+++ client/src/components/Nav/Navbar/nav.js
@@ -0,0 +1,16 @@
+export default funciton Nav() {
+    return (
+<nav className="nav">
+    <a href="Home" className="site-name">Roll With It</a>
+    <ul>
+        <li> 
+        <a href="/login">Login</a>
+        </li>
+        <li>
+        <a href="/signup">Sign Up</a>
+        </li>
+    </ul>
+    </nav>
+
+    )
+}
