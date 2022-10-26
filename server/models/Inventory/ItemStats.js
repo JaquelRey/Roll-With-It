b98983d714067d5159d5b47a8dc09922cb8495c9
jaquel <jaquelrey@gmail.com>
2022-10-26 16:24:33 -0400

    update: itemstats model to npm for json

--- server/models/Inventory/ItemStats.js
+++ server/models/Inventory/ItemStats.js
@@ -2,9 +2,16 @@ const { Schema, model } = require('mongoose');
 
 // Schema to create ItemStats model
 // Will have several different results for keys and values when populated
-const itemStatsSchema = new Schema(
-    { any: {} }
-  );
+import GraphQLJSON, { GraphQLJSONObject } from 'graphql-type-json';
+
+const itemStatsSchema = new GraphQLObjectType({
+  name: 'ItemStats',
+
+  fields: {
+    itemstat: { type: GraphQLJSON },
+    myObject: { type: GraphQLJSONObject },
+  },
+});
 
 const ItemStats = model('ItemStats', itemStatsSchema);
 
